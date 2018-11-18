f670ea56f33f51931e843802f54e5e2c4b4c7d15
EthoKurza <36766454+EthoKurza@users.noreply.github.com>
2018-11-18 15:01:34 -0500

    Update posixThread.c

--- posixThread.c
+++ posixThread.c
@@ -1,8 +1,20 @@
 include "buffer.h"
 	int main(int argc, char *argv[]) {
+		
+		//Check if enough arguments are entered
+		if (argc < 4){
+			printf("not enough arguments entered \n");
+		}
 		/* 1. Get command line arguments  argv[1],argv[2],argv[3] */
-		/* 2. Initialize buffer *//* 3. Create producer thread(s) */
+		
+		/* 2. Initialize buffer */
+	
+		/* 3. Create producer thread(s) */
+	
 		/* 4. Create consumer thread(s) */
+	
 		/* 5. Sleep */
+	
 		/* 6. Exit */
-	}
+	
+	}//end of main

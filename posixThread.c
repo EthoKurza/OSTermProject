eab93ee2ac621ddcb92d04435d824f45c1a68bad
PLLman <45151404+PLLman@users.noreply.github.com>
2018-11-18 16:12:15 -0500

    Overall Skeleton
    We will begin to create our program using this as our main skeleton.

--- posixThread.c
+++ posixThread.c
@@ -1,6 +1,41 @@
 #include <stdlib.h>
 #include "buffer.h"
 
+int insert_item(buffer item);
+int remove_item(buffer item);
+
+void *producer(void *param){
+	/*
+	buffer item item;
+
+ 	while (true) {
+ 		//sleep for a random period of time
+ 		sleep(...);
+ 		//generate a random number
+ 		item = rand();
+ 		if (insert item(item))
+ 			fprintf("report error condition");
+ 		else
+ 			printf("producer produced %d\n",item);
+ 	}
+	*/
+}
+
+void *consumer(void *param){
+	/*
+	buffer item item;
+
+ 	while (true) {
+ 	//sleep for a random period of time
+ 	sleep(...);
+ 	if (remove item(&item))
+ 		fprintf("report error condition");
+ 	else
+ 		printf("consumer consumed %d\n",item);
+ 	}
+	*/
+}
+
 int main(int argc, char *argv[]) {
 
 	//Check if enough arguments are entered
@@ -10,37 +45,52 @@ int main(int argc, char *argv[]) {
 	}
 	
 	//declare and inititalize variables
-	int sleepLength=0, producerNum=0, consumerLength=0
 
 	/* 1. Get command line arguments  argv[1],argv[2],argv[3] */
-	sleepLength = atoi(argv[1]);
-	producerNum = atoi(argv[2]);
-	consumerLength = atoi(argv[3]);
+	int sleepLength = atoi(argv[1]);
+	int producerNum = atoi(argv[2]);
+	int consumerNum = atoi(argv[3]);
 	
 	
 	/* 2. Initialize buffer */
 	buffer item_buffer[BUFFER SIZE];
-
-	//insert item into buffer 
-	int insert_item (buffer item item) {
-		/* inser item into buffer 
-		return 0 if successful, otherwise
-		return -1 indicating an error condition */
-	}
-
-	//remove item from buffer
-	int remove_item(buffer item *item) {
-		/* remove an object from buffer 
-		placing it in item
-		return 0 if successful, otherwise
-		return -1 indicating an error condition */
-	}
+	
+	pthread_t tid;
+	
 	/* 3. Create producer thread(s) */
+	for(int i = 0; i > producerNum; i++){
+		pthread_create(&tid, NULL, producer, NULL);
+	}
 
 	/* 4. Create consumer thread(s) */
+	for(int i = 0; i > consumerNum; i++){
+		pthread_create(&tid, NULL, consumer, NULL);
+	}
 
 	/* 5. Sleep */
+	sleep(sleepLength);
 
 	/* 6. Exit */
-
+	return 0;
 }//end of main
+
+//insert item into buffer 
+int insert_item (buffer item item) {
+	/* insert item into buffer 
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
+
+//remove item from buffer
+int remove_item(buffer item *item) {
+	/* remove an object from buffer 
+	placing it in item
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
+
+/*
+	Regular errors should be printed to stderr() by means of fprintf(). The choice of when to terminate the program on specific errors is left to you.
+	
+	Failed system calls should use the perror() function for reporting error messages.
+*/
